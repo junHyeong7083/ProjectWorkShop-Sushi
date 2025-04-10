0c0ab549eaf9e375ce982dd3e5b6093646c97607
junHyeong7083 <parkjunhyeong0902@gmail.com>
2025-04-10 17:22:45 +0900

    PalletTown
    태초마을 시스템

--- Assets/02. Script/MapPattern/ThrowPattern.cs
+++ Assets/02. Script/MapPattern/ThrowPattern.cs
@@ -5,6 +5,7 @@ public class ThrowPattern : MonoBehaviour
 {
     bool isThrow = false;
 
+    #region SettingValue
     [Header("회전속도 값이 오를수록 빠르게 회전합니다")]
     public float rotateSpeed = 1440f; // 초당 1440도 (매우 빠르게)
 
@@ -26,6 +27,23 @@ public class ThrowPattern : MonoBehaviour
 
     Quaternion originalRotation;
 
+    [Space(20)]
+    [Header("모델의 콜라이더")]
+    [SerializeField] BoxCollider fbxCollider;
+
+    [Space(20)]
+    [Header("던질 콜라이더")]
+    [SerializeField] BoxCollider throwCollider;
+
+    [Space(20)]
+    [Header("태초마을 위치좌표")]
+    [SerializeField] Transform PalletTownTransform;
+
+
+    [Space(20)]
+    [Header("던져질때 꼭짓점 위치")]
+    [SerializeField] Transform Pointtransform;
+    #endregion
     private void Start()
     {
         originalRotation = transform.rotation;
@@ -33,28 +51,66 @@ public class ThrowPattern : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.contacts.Length > 0)
         {
-            if (!isThrow)
+            foreach (var contact in collision.contacts)
             {
-                Throw();
-
-                // 플레이어를 밀어내기
-                Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
-                if (playerRb != null)
+                if (contact.thisCollider == throwCollider)
                 {
-                    Vector3 throwDirection = (collision.transform.position - transform.position).normalized;
-                    playerRb.AddForce(throwDirection * throwPower, ForceMode.Impulse);
+                    if (collision.gameObject.CompareTag("Player"))
+                    {
+                        if (!isThrow)
+                        {
+                            Throw();
+
+                            Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
+                            if (playerRb != null)
+                            {
+                                // 기존 AddForce는 지우고
+                                StartCoroutine(MoveAlongCurve(playerRb));
+                            }
+                        }
+                    }
+                    break; // 한번 처리했으면 끝내기
                 }
             }
         }
     }
+
+
 
     void Throw()
     {
         isThrow = true;
         StartCoroutine(ThrowSystem());
     }
+
+    IEnumerator MoveAlongCurve(Rigidbody playerRb)
+    {
+        Vector3 startPos = playerRb.position;
+        Vector3 controlPos = Pointtransform.position;
+        Vector3 endPos = PalletTownTransform.position;
+
+        float duration = 2.0f; // 이동 총 시간
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            // 2차 베지어 곡선 공식
+            Vector3 bezierPos = Mathf.Pow(1 - t, 2) * startPos
+                              + 2 * (1 - t) * t * controlPos
+                              + Mathf.Pow(t, 2) * endPos;
+
+            playerRb.MovePosition(bezierPos); // Rigidbody로 부드럽게 이동
+            elapsed += Time.fixedDeltaTime;   // FixedUpdate 시간에 맞춰 진행
+            yield return new WaitForFixedUpdate();
+        }
+
+        playerRb.MovePosition(endPos); // 최종 위치 보정
+    }
+
+
 
     IEnumerator ThrowSystem()
     {
